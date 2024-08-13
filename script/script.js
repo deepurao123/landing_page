111ed0f123b0049d49093d7e5a5774ee0a1fe341
Deepu Kumar Rao <129026045+deepurao123@users.noreply.github.com>
2024-08-13 15:54:15 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,13 @@
+// scripts.js
+document.addEventListener("DOMContentLoaded", () => {
+  const navbarToggler = document.getElementById("navbar-toggler");
+  const navbarMenu = document.getElementById("navbar-menu");
+
+  navbarToggler.addEventListener("click", () => {
+    if (navbarMenu.style.display === "flex") {
+      navbarMenu.style.display = "none";
+    } else {
+      navbarMenu.style.display = "flex";
+    }
+  });
+});
